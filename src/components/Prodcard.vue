692c3b3b7da54fb6342d245c9980764aecefd89d
Roberto <robertoanfuso98@gmail.com>
2021-07-07 23:32:30 +0200

    Ho problemi con l'esercizio

---
+++ src/components/Prodcard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Prodotti">
+      <div>
+         <div>ciao</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Prodcard',
+    props:["prod"]
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,23 +2,57 @@
   <div>
       <main>
           <h1>--STIAMO LAVORANDO PER VOI--</h1>
+            <Prodcard
+            v-for="(item,index) in links" 
+            :key="index"
+            :prod="item"/>
+            
+          
       </main>
   </div>
 </template>
 
 <script>
+import Prodcard from '@/components/Prodcard.vue'
+import Pippo from '@/components/Pippo.vue'
+import array from '@/data/array.js'
 export default {
-
+    name:"product",
+    component:{
+        Prodcard,
+        Pippo
+    },
+     data(){
+        return{
+         links:array
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     main{
-        height: 100px;
+        
         background-color: black;
         h1{
             color: white;
             line-height: 100px;
+        }
+        ul{
+            display: flex;
+            justify-content: space-evenly;
+            li{
+                display: inline-block;
+                margin: 20px 0px;
+                color: white;
+                text-align: center;
+                .fumetto{
+                    background-color: green;
+                    height: 150px;
+                    width: 150px;
+                }
+
+            }
         }
     }
 </style>
